3f861c60e207499039c07976d9dc0a8dfbf005de
h20smallnoob <h20.smallnoob@gmail.com>
2026-03-08 18:26:58 +1100

    added new stuff

--- GameManager.gd
+++ GameManager.gd
@@ -1,9 +1,12 @@
 extends Node
 
-
 var last_checkpoint_pos : Vector2 = Vector2.ZERO
 var camera_at_checkpoint: bool = false
 
-var player_boost = 1
-var speed_coin_mult = 1
-var jump_coin_mult = 1
+var player_boost = 1.0
+var speed_coin_mult = 1.0
+var jump_coin_mult = 1.0
+
+
+var collected_speed_coins = []
+var collected_jump_coins = []
