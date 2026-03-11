d5bb9c252a77bae91b5b35d185b4210d53447ba2
h20smallnoob <h20.smallnoob@gmail.com>
2026-03-11 20:57:01 +1100

    Added new content

--- GameManager.gd
+++ GameManager.gd
@@ -7,17 +7,15 @@ var player_boost = 1.0
 var speed_coin_mult = 1.0
 var jump_coin_mult = 1.0
 var scene_num = 0
-
+var crystal_amount = 0
 
 var collected_speed_coins = []
 var collected_jump_coins = []
 func respawn_player(player_instance):
 	if last_checkpoint_pos != Vector2.ZERO:
-		# Move the player to the saved position
 		player_instance.global_position = last_checkpoint_pos
 		
-		# If the camera needs to snap to the checkpoint
 		if camera_at_checkpoint:
 			var camera = player_instance.get_node_or_null("Camera2D")
 			if camera:
-				camera.reset_smoothing() # Prevents the camera from sliding across the map
+				camera.reset_smoothing() 
