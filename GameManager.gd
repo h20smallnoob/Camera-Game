d44a72bd6ccd1eaf61adacdfae359e8d414ca298
h20smallnoob <h20.smallnoob@gmail.com>
2026-03-20 12:47:16 +1100

    Added new content
    Added new level, added boss level, fixed levels being impossible without coins

--- GameManager.gd
+++ GameManager.gd
@@ -2,7 +2,8 @@ extends Node
 
 var last_checkpoint_pos : Vector2 = Vector2.ZERO
 var camera_at_checkpoint: bool = false
-
+var boss_alive = 1
+var bounce_out = 0
 var player_boost = 1.0
 var speed_coin_mult = 1.0
 var jump_coin_mult = 1.0
@@ -11,6 +12,7 @@ var crystal_amount = 0
 var double_jump = 0
 var collected_speed_coins = []
 var collected_jump_coins = []
+
 func respawn_player(player_instance):
 	if last_checkpoint_pos != Vector2.ZERO:
 		player_instance.global_position = last_checkpoint_pos
